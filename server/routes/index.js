b2c3e23bac1c34495c822aa2b4949dab62818978
Daniel Owen van Dommelen <daniel.van.dommelen@gmail.com>
2018-03-01 16:37:19 +0100

    Moving routes and code to a more logical place, and structuring views in directories

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,44 +1,8 @@
 var express = require('express');
-var router = express.Router();
-
-const knex = require('../db/knex');
-
-function validUser(user) {
-  return typeof user.first_name == 'string' &&
-                user.first_name.trim() != '' &&
-         typeof user.last_name == 'string' &&
-                user.last_name.trim() != '';
-}
+var router  = express.Router();
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Credit Carma' });
-});
-
-router.post('/users', function(req, res) {
-  if(validUser(req.body)) {
-    const now        = new Date().toLocaleString();
-    const date_day   = '01';
-    const date_month = req.body.expires_on.split('/')[0];
-    const date_year  = '20' + req.body.expires_on.split('/')[1];
-
-    const user = {
-      first_name:  req.body.first_name,
-      last_name:   req.body.last_name,
-      card_number: req.body.card_number,
-      cvv:         req.body.cvv,
-      expires_on:  date_year + '/' + date_month + '/' + date_day,
-      created_at:  now,
-      updated_at:  now
-    };
-
-    knex('users').insert(user, 'id').then(ids => {
-      const id = ids[0];
-      res.redirect(`/users/${id}`);
-    });
-  }
-  else {
-
-  }
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+const knex  = require('../db/knex');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+function validUser(user) {
+  return typeof user.first_name == 'string' &&
+                user.first_name.trim() != '' &&
+         typeof user.last_name == 'string' &&
+                user.last_name.trim() != '';
+}
+
+router.get('/:id', function(req, res) {
+  const id = req.params.id;
+
+  if(typeof(id) != 'undefined') {
+    knex('users').select().where('id', id).first().then(user => {
+      res.render('users/show', {user: user});
+    });
+  }
+  else {
+    res.status(404);
+    res.render('error', {
+      message: 'User not found'
+    });
+  }
+});
+
+router.post('/', function(req, res) {
+  if(validUser(req.body)) {
+    const now        = new Date().toLocaleString();
+    const date_day   = '01';
+    const date_month = req.body.expires_on.split('/')[0];
+    const date_year  = '20' + req.body.expires_on.split('/')[1];
+
+    const user = {
+      first_name:  req.body.first_name,
+      last_name:   req.body.last_name,
+      card_number: req.body.card_number,
+      cvv:         req.body.cvv,
+      expires_on:  date_year + '/' + date_month + '/' + date_day,
+      created_at:  now,
+      updated_at:  now
+    };
+
+    knex('users').insert(user, 'id').then(ids => {
+      const id = ids[0];
+      res.redirect(`/users/${id}`);
+    });
+  }
+  else {
+    res.status(422);
+    res.render('error', {
+      message: 'Invalid user'
+    });
+  }
 });
 
 module.exports = router;
